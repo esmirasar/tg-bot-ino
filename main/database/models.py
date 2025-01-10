a44588e19163b2265636e7645183e09f4a69b700
Esmira <esmirasar@yandex.ru>
2025-01-10 18:02:23 +0400

    creation models

--- main/database/models.py
+++ main/database/models.py
@@ -10,8 +10,43 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
-
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     telegram_id: Mapped[int] = mapped_column(BigInteger(), unique=True)
+    telegram_name: Mapped[str] = mapped_column(String(70))
+    telegram_username: Mapped[str] = mapped_column(String(32), unique=True)
+
+    categories: Mapped[List["Category"]] = relationship("Category", back_populates="user", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"User(id={self.id!r}, telegram_id={self.telegram_id!r}, telegram_name={self.telegram_name!r}, telegram_username={self.telegram_username!r})"
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    language: Mapped[str] = mapped_column(String(15))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    user: Mapped["User"] = relationship("User", back_populates="categories")
+    words: Mapped[List["Words"]] = relationship("Words", back_populates="category", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"Category(id={self.id}, language={self.language}, user_id={self.user_id})"
+
+
+class Words(Base):
+    __tablename__ = "words"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    word: Mapped[Optional[str]] = mapped_column(String())
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
+    collocation: Mapped[Optional[str]] = mapped_column(String())
+    translation: Mapped[str] = mapped_column(String())
+
+    category: Mapped["Category"] = relationship("Category", back_populates="words")
+
+    def __repr__(self):
+        return f"Words(id={self.id}, word={self.word}, category_id={self.category_id}, collocation={self.collocation}, translation={self.translation})"
